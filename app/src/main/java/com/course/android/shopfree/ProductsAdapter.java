233d9d57cbe9199eb67ebecb633da69b375e9d9c
aya.khalaf92 <aya.khalaf92@yahoo.com>
2015-03-14 18:02:49 +0200

    add new

---
+++ app/src/main/java/com/course/android/shopfree/ProductsAdapter.java
@@ -0,0 +1,7 @@
+package com.course.android.shopfree;
+
+/**
+ * Created by Aya on 3/14/2015.
+ */
+public class ProductsAdapter {
+}
---
+++ app/src/main/java/com/course/android/shopfree/User.java
@@ -0,0 +1,14 @@
+package com.course.android.shopfree;
+
+/**
+ * Created by Aya on 3/14/2015.
+ */
+public class User {
+    private String Product_type;
+
+    public User(String name){
+        Product_type = name;
+
+    }
+    public String getName(){return  Product_type;}
+}
